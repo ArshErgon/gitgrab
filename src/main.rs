43783b8402e4f1ded23698a18aea3bb0e8552bb7
ArshErgon <arshergon@gmail.com>
2023-03-10 09:40:18 +0430

    refactor: :art: deleted unused imports

--- src/main.rs
+++ src/main.rs
@@ -10,22 +10,17 @@ mod graph {
     pub mod graph_maker;
 }
 
-use std::env;
-use std::fs::{self};
-use std::io::prelude::*;
-use std::path::Path;
 
 fn main() {
-    input::cli_input();
-    start_the_project();
+    let (username, secret_key) = input::cli_input();
+    start_the_project(username, secret_key);
 }
 
 // fix the below overcomplicated code.
 // as you can see secretkey is being used in 3 different function, make it good.
 // filter out non-useful variables and functions
 // :: dont ask for
-fn start_the_project() {
-    let (arg, secret_key) = input::show_user_info();
+fn start_the_project(arg: String, secret_key: String) {
     let header_git_data = profile_header::start_header_info(arg.as_str(), secret_key.clone());
     let counter_data = get_detailed_view::main_view_start(arg.to_string(), secret_key.clone());
     let filter_data = counter_data;
